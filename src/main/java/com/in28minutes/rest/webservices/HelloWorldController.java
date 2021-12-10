e109f0bdcf0574832d75a6c0111e474d87b1a661
Imran107 <imran.itim@gmail.com>
2021-12-10 17:25:55 +0500

    Add cross origin webservice calls compatability and throw error in webservice to handle

--- src/main/java/com/in28minutes/rest/webservices/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/HelloWorldController.java
@@ -1,9 +1,12 @@
 package com.in28minutes.rest.webservices;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins="http://localhost:3000")
 public class HelloWorldController {
 	
 	@GetMapping(path="/hello-world")
@@ -13,7 +16,13 @@ public class HelloWorldController {
 	
 	@GetMapping(path="hello-world-bean")
 	public HelloWorldBean helloWorldBean(){
-		return new HelloWorldBean("Hello World");
+		return new HelloWorldBean("Hello World Bean");
+	}
+	
+	@GetMapping(path="hello-world-bean/path-variable/{name}")
+	public HelloWorldBean helloWorldBeanPathVariable(@PathVariable String name){
+		throw new RuntimeException("Something went wrong");
+		//return new HelloWorldBean(String.format("Hello World, %s", name));
 	}
 
 }
