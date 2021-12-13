ac08e2d5e65b6a346c8747f431aab5c90b90de68
Imran107 <imran.itim@gmail.com>
2021-12-13 22:18:01 +0500

    Implement delete Todo Service

--- src/main/java/com/in28minutes/rest/webservices/todo/TodoResource.java
+++ src/main/java/com/in28minutes/rest/webservices/todo/TodoResource.java
@@ -3,7 +3,9 @@ package com.in28minutes.rest.webservices.todo;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,4 +21,14 @@ public class TodoResource {
 	public List<Todo> getAllTodos(@PathVariable String username){
 		return service.findAll();
 	}
+	
+	@DeleteMapping("delete/{username}/todos/{id}")
+	public ResponseEntity<Void> deleteTodo(@PathVariable String username, @PathVariable long id){
+		Todo todo = service.deleteTodo(id);
+		if(todo != null){
+			return ResponseEntity.noContent().build();
+		}
+		return ResponseEntity.notFound().build();
+	}
+	
 }
